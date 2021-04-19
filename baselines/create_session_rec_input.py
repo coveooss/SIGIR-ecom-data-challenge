d47e5c3d0934b9dc4405540e573928f3a851d313
jacopo tagliabue <jacopo.tagliabue@tooso.org>
2021-04-19 13:44:25 -0400

    patch baseline folder with JF fix!

--- baselines/create_session_rec_input.py
+++ baselines/create_session_rec_input.py
@@ -1,95 +1,90 @@
 """
-The script takes a recommendation test data file, and convert it into "prepared" data that
-can be directly consumed by session_rec repo to run baselines for recommendation task
+The script takes recommendation train and test data files, and convert thrm into "prepared" data that
+can be directly consumed by session_rec repo to run baselines for the recommendation task.
 """
+
+import json
+from pathlib import Path
+import pickle
 
 import pandas as pd
 
-import os
-import json
-import pickle
+# This script uses a subset of session IDs to sub-sample the training data.
+# Please change the default 1% ratio below to suit your needs.
+TRAIN_RATIO = 0.01
 
-TRAIN_PATH = './session_rec_sigir_data/train/browsing_train.csv'
-TEST_PATH = './session_rec_sigir_data/test/rec_test.json'
+TRAIN_PATH = Path('./session_rec_sigir_data/train/browsing_train.csv')
+TEST_PATH = Path('./session_rec_sigir_data/test/rec_test_sample.json')
 
-PREPARED_FOLDER = './session_rec_sigir_data/prepared'
-if not os.path.exists(PREPARED_FOLDER):
-    os.makedirs(PREPARED_FOLDER)
-PREPARED_TRAIN_PATH = os.path.join(PREPARED_FOLDER, 'sigir_train_full.txt')
-PREPARED_TEST_PATH = os.path.join(PREPARED_FOLDER, 'sigir_test.txt')
-ITEM_I2S_MAP_PATH = os.path.join(PREPARED_FOLDER, 'item_i2s.p')
+PREPARED_FOLDER = Path('./session_rec_sigir_data/prepared')
+PREPARED_FOLDER.mkdir(parents=True, exist_ok=True)
+
+PREPARED_TRAIN_PATH = PREPARED_FOLDER / 'sigir_train_full.txt'
+PREPARED_TEST_PATH = PREPARED_FOLDER / 'sigir_test.txt'
+ITEM_LABEL_ENCODING_MAP_PATH = PREPARED_FOLDER / 'item_label_encoding.p'
 
 SessionId = 'SessionId'
 ItemId = 'ItemId'
 Time = 'Time'
 
 
-def get_converted_to_int(series: pd.Series):
-    '''
-    Consume a item dataframe series (string) to
-    generate string<->integer mapping and
-    a new series with integer IDs
+def label_encode_series(series: pd.Series):
+    """
+    Applies label encoding to a Pandas series and returns the encoded series,
+    together with the label to index and index to label mappings.
 
-    :param series: input dataframe series
-    :return: string-integer mappng and the new transformed series
-    '''
-    series_strings = set(series.unique())
-    s2i_map = {s: idx for idx, s in enumerate(series_strings)}
-    i2s_map = {v: k for k, v in s2i_map.items()}
-    return s2i_map, i2s_map, series.map(s2i_map)
+    :param series: input Pandas series
+    :return: Pandas series with label encoding, label-integer mapping and integer-label mapping.
+    """
+    labels = set(series.unique())
+    label_to_index = {l: idx for idx, l in enumerate(labels)}
+    index_to_label = {v: k for k, v in label_to_index.items()}
+    return series.map(label_to_index), label_to_index, index_to_label
 
 
 print("\n ========================= generate 'prepared' train ========================")
 train_data_df = pd.read_csv(TRAIN_PATH)
 
-# use subset of session IDs to sub-sample training data
-# here it is set to 1% of train data
-train_ratio = 1
-if train_ratio:
-    session_ids = set(train_data_df['session_id_hash'].unique())
-    train_cutoff = int(len(session_ids) * train_ratio / 100)
-    train_session_ids = list(session_ids)[:train_cutoff]
-    train_data_df = train_data_df[train_data_df['session_id_hash'].isin(train_session_ids)]
+session_ids = set(train_data_df['session_id_hash'].unique())
+train_cutoff = int(len(session_ids) * TRAIN_RATIO)
+train_session_ids = list(session_ids)[:train_cutoff]
+train_data_df = train_data_df[train_data_df['session_id_hash'].isin(train_session_ids)]
 
-# filter out `remove from cart` events to feed to session_rec as positive signals
-train_data_no_remove = train_data_df[train_data_df['product_action'] != 'remove']
+# Filter out:
+# * `remove from cart` events to avoid feeding them to session_rec as positive signals
+# * rows with null product_sku_hash
+# * sessions with only one action
+train_data_df = train_data_df[train_data_df['product_action'] != 'remove']
+train_data_df = train_data_df.dropna(subset=['product_sku_hash'])
+train_data_df['session_len_count'] = train_data_df.groupby('session_id_hash')['session_id_hash'].transform('count')
+train_data_df = train_data_df[train_data_df['session_len_count'] >= 2]
+
 # sort by session, then timestamp
-train_data_no_remove = train_data_no_remove.sort_values(['session_id_hash', 'server_timestamp_epoch_ms'],
-                                                        ascending=True)
-# got time!
-train_data_no_remove[Time] = (train_data_no_remove.server_timestamp_epoch_ms / 1000)
-# map SessionId to int
-train_data_no_remove[SessionId] = \
-train_data_no_remove.groupby([train_data_no_remove.session_id_hash]).grouper.group_info[0]
+train_data_df = train_data_df.sort_values(['session_id_hash', 'server_timestamp_epoch_ms'], ascending=True)
 
-# remove product_sku_hash null
-train_data_no_remove = train_data_no_remove.dropna(subset=['product_sku_hash'])
+# Encode labels with integers
+item_id_int_series, item_label_to_index, item_index_to_label = label_encode_series(train_data_df.product_sku_hash)
+item_string_set = set(item_label_to_index.keys())
 
-# remove all sessions having only 1 event
-train_data_no_remove['session_len_count'] = train_data_no_remove.groupby([SessionId])[Time].transform('count')
-train_data_no_remove = train_data_no_remove[train_data_no_remove['session_len_count'] >= 2]
+# Add tokenized session ID, tokenized item ID, and seconds since epoch time.
+train_data_df[SessionId] = train_data_df.groupby([train_data_df.session_id_hash]).grouper.group_info[0]
+train_data_df[Time] = train_data_df.server_timestamp_epoch_ms / 1000
+train_data_df[ItemId] = item_id_int_series
 
-# get training item set
-item_string_set = set(train_data_no_remove.product_sku_hash.unique())
-# map ItemId to int
-item_id_s2i_map, item_id_i2s_map, item_id_int_series = get_converted_to_int(train_data_no_remove.product_sku_hash)
-pickle.dump(item_id_i2s_map, open(ITEM_I2S_MAP_PATH, "wb"))
+# Get final dataframe
+final_train_df = train_data_df[[SessionId, ItemId, Time]]
 
-train_data_no_remove[ItemId] = item_id_int_series
-
-# get final df
-final_train_df = train_data_no_remove[[SessionId, ItemId, Time]]
-
-# generate csv
+# Generate CSV and label encoder
 final_train_df.to_csv(PREPARED_TRAIN_PATH, sep='\t', index=False)
+pickle.dump(item_index_to_label, ITEM_LABEL_ENCODING_MAP_PATH.open(mode='wb'))
 print("Done generating 'prepared' for training")
 
 print("\n ========================= generate 'prepared' test ========================")
-with open(TEST_PATH) as f:
+with TEST_PATH.open() as f:
     test_data = json.load(f)
 
 test_output = []
-dummy_label_item = list(item_string_set)[0]
+dummy_label_item = next(iter(item_string_set))
 for idx, query_label in enumerate(test_data):
     query = query_label['query']
 
@@ -121,10 +116,9 @@ for idx, query_label in enumerate(test_data):
         test_output = test_output + cleaned_query_events
         test_output = test_output + [l_event]
 
-# convert list to df
+# Create the final dataframe and apply label encoding.
 test_output_df = pd.DataFrame(test_output)
-# map string to int for ItemId
-test_output_df[ItemId] = test_output_df.ItemId.map(item_id_s2i_map)
+test_output_df[ItemId] = test_output_df.ItemId.map(item_label_to_index)
 test_output_df.to_csv(PREPARED_TEST_PATH, sep='\t', index=False)
 print("Done generating 'prepared' for testing")
 
